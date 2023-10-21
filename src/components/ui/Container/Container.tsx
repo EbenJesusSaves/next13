9286a608858b84c62a161799446440b5cff0e748
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-10-21 21:20:44 +0000

    added container 😂😂

---
+++ src/components/ui/Container/Container.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Container: FC<Props> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Container;
--- src/components/ui/Hero/Hello.tsx
+++ src/components/ui/Hero/Hello.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import s from "./Hello.module.css";
 import Link from "next/link";
+import { Container } from "..";
 interface Props {
   headline: string;
   description: string;
@@ -8,17 +9,19 @@ interface Props {
 
 const Hello: FC<Props> = ({ headline, description }) => {
   return (
-    <div className="bg-black  ">
-      <div className={s.root}>
-        <h2 className={s.headline}>{headline}</h2>
+    <Container>
+      <div className="bg-black  ">
+        <div className={s.root}>
+          <h2 className={s.headline}>{headline}</h2>
+        </div>
+        <div className={s.description}>
+          <p>{description}</p>
+        </div>
+        <Link className={s.link} href={"/"}>
+          read it here dude
+        </Link>
       </div>
-      <div className={s.description}>
-        <p>{description}</p>
-      </div>
-      <Link className={s.link} href={"/"}>
-        read it here dude
-      </Link>
-    </div>
+    </Container>
   );
 };
 
