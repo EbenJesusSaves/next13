1e1254586d18bb730ff953631bb82a4b4b982bf1
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-11-11 22:36:14 +0000

    added store and wish list

--- src/components/common/Usernav/Usernav.tsx
+++ src/components/common/Usernav/Usernav.tsx
@@ -6,24 +6,26 @@ import Link from "next/link";
 import { LineItem } from "@/framework/common/types/cart";
 
 const Usernav: FC = () => {
-  //   const { openSidebar } = useUI();
-  //   const { data } = useCart();
+  // const { openSidebar } = useUI();
+  // const { data } = useCart();
 
-  //   const itemsCount =
-  //     data?.lineItems.reduce((count: number, item: LineItem) => {
-  //       return count + item.quantity;
-  //     }, 0) ?? 0;
+  // const itemsCount =
+  //   data?.lineItems.reduce((count: number, item: LineItem) => {
+  //     return count + item.quantity;
+  //   }, 0) ?? 0;
 
   return (
     <nav>
       <ul className={s.list}>
         <li className={s.item}>
-          {/* <Cart onClick={openSidebar} /> */}
+          Cart
+          {/* <Cart onClick={openSidebar} />  */}
           {/* {itemsCount > 0 && <span className={s.bagCount}>{itemsCount}</span>} */}
         </li>
         <li className={s.item}>
           <Link href="/wishlist">
-            <div>{/* <Heart /> */}</div>
+            Wishlist
+            {/* <div> <Heart /> </div> */}
           </Link>
         </li>
       </ul>
