f46292d1bfc203710dd1ebb97b2d19598c1d374a
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-11-29 23:51:44 +0000

    added slug comp

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,8 +1,26 @@
 import { Layout } from "@/components/common";
+import { GetStaticPaths } from "next";
 import React from "react";
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { slug: "cool-hat" } },
+      { params: { slug: "lightweight-jacket" } },
+      { params: { slug: "t-shirt" } },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps = () => {};
+
 const ProductSlug = () => {
-  return <div>[slug]</div>;
+  return (
+    <Layout>
+      <div>[slug] apnkye krakra asoro ne fum</div>
+    </Layout>
+  );
 };
 
 export default ProductSlug;
